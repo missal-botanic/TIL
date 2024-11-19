1c37cdf850e02f36d12abc7cf4a0afebf17f15aa
missal-botanic <missal-botanic-0y@icloud.com>
2024-11-19 22:07:38 +0900

    2024-11-19 22:07:38

--- git.py
+++ git.py
@@ -1,3 +1,8 @@
- git add .
- git commit -m "$(date +'%Y-%m-%d %H:%M:%S')"
- git push origin main
+git add .
+git commit -m "$(date +'%Y-%m-%d %H:%M:%S')"
+git push origin main
+
+
+git reset --soft HEAD~1
+
+git reset
---
+++ test.py
@@ -0,0 +1,11 @@
+import torch
+import torchvision
+import torchaudio
+import torchtext
+
+print(f"torch version: {torch.__version__}")
+print(f"torchvision version: {torchvision.__version__}")
+print(f"torchaudio version: {torchaudio.__version__}")
+print(f"torchtext version: {torchtext.__version__}")
+
+print(f"CUDA version: {torch.version.cuda}")
